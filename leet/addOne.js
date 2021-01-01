7d97b4501fa840b2813baa976881aef477056081
mbelesiu <matt.belesiu@gmail.com>
2021-01-01 09:46:23 -0800

    adding another one :)

---
+++ leet/addOne.js
@@ -0,0 +1,41 @@
+// Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+// The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+// You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+/*
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for(let i = digits.length - 1; i >= 0; i--){
+      digits[i] += 1;
+      if(digits[i] === 10){
+        digits[i] = 0
+      } else {
+          break
+      }
+      if (digits[0] === 0 && i === 0){
+      digits.unshift(1);
+  }
+  }
+
+
+  return digits;
+};
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Example 3:
+
+// Input: digits = [0]
+// Output: [1]
