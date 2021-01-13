bc31136d632ad3c2d43f22ea9c74b32a326d77de
mbelesiu <matt.belesiu@gmail.com>
2021-01-13 08:14:00 -0800

    coverting arrays to bst so fun

---
+++ leet/convertSortedArrToBST.js
@@ -0,0 +1,41 @@
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+  if (nums.length === 1) {
+    return new TreeNode(nums[0]);
+  }
+  const halfway = Math.floor(nums.length / 2)
+  return new TreeNode(nums[halfway], sortedArrayToBST(nums.slice(0, halfway)), sortedArrayToBST(nums.slice(halfway + 1)));
+
+};
+
+
+// Example:
+
+// Given the sorted array: [-10,-3,0,5,9],
+
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
