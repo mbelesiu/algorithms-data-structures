cbf73aba61a73ce400a198d68cf8e7d3f06a6c81
mbelesiu <matt.belesiu@gmail.com>
2021-01-29 09:34:38 -0800

    NOW IN 2D

--- leet/search2dSortedArray.js
+++ leet/search2dSortedArray.js
@@ -24,6 +24,27 @@ var searchMatrix = function (matrix, target) {
   return false;
 };
 
+//or
+
+var searchMatrix = function (matrix, target) {
+  const row = matrix.length;
+  const col = matrix[0].length;
+
+  for (let i = 0; i < row; i++) {
+    if (matrix[i][col - 1] >= target) {
+      for (let j = 0; j < col; j++) {
+        if (matrix[i][j] > target) {
+          return false;
+        } else if (matrix[i][j] === target) {
+          return true;
+        }
+      }
+    }
+
+  }
+  return false;
+};
+
 // Example 1:
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 // Output: true
