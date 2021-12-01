a19a71384257c9e2682705ecc5f222213a2d86e3
Matt Belesiu <matt.belesiu@gmail.com>
2021-11-30 16:49:36 -0800

    same tree, differnt forest

---
+++ leet/sameTree.py
@@ -0,0 +1,22 @@
+# // Given two binary trees, write a function to check if they are the same or not.
+
+# // Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p and q or not q and p:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
