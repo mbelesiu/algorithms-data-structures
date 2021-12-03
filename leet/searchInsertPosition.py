c6ca89055c6122e2fc4d6d0f9192b48e9a9befb2
Matt Belesiu <matt.belesiu@gmail.com>
2021-12-03 15:45:55 -0800

    seach and insert position. Target aquired. Position successfully inserted

---
+++ leet/searchInsertPosition.py
@@ -0,0 +1,30 @@
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for x in range(len(nums)):
+            if target <= nums[x]:
+                return x
+
+        return x+1
+
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+# Example 4:
+
+# Input: nums = [1,3,5,6], target = 0
+# Output: 0
+# Example 5:
+
+# Input: nums = [1], target = 0
+# Output: 0
