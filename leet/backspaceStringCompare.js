6ca178375a5d66f5b363e64960ec47e1b87fdd93
Matt Belesiu <matt.belesiu@gmail.com>
2022-01-23 12:42:31 -0800

    fixed formatting

--- leet/backspaceStringCompare.js
+++ leet/backspaceStringCompare.js
@@ -6,46 +6,46 @@
  * @param {string} t
  * @return {boolean}
  */
- var backspaceCompare = function(s, t) {
+var backspaceCompare = function (s, t) {
 
-  let i = s.length - 1;
-  let j = t.length -1;
-  let sSkip = 0;
-  let tSkip = 0;
+    let i = s.length - 1;
+    let j = t.length - 1;
+    let sSkip = 0;
+    let tSkip = 0;
 
-  while(i >=0 || j >= 0) {
-      while(i>=0){
-          if(s[i] === '#') {
-              sSkip++;
-              i--;
-          } else if (sSkip > 0) {
-              sSkip--;
-              i--;
-          } else {
-              break;
-          }
-      }
-      while(j>=0){
-          if(t[j] === '#') {
-              tSkip++;
-              j--;
-          } else if (tSkip > 0) {
-              tSkip--;
-              j--;
-          } else {
-              break;
-          }
-      }
-      if(i>=0 && j >=0 && s[i] != t[j]){
-          return false
-      }
-      if((i>=0) != (j>=0)){
-         return false
-         }
-      i--;
-      j--;
-  }
-  return true
+    while (i >= 0 || j >= 0) {
+        while (i >= 0) {
+            if (s[i] === '#') {
+                sSkip++;
+                i--;
+            } else if (sSkip > 0) {
+                sSkip--;
+                i--;
+            } else {
+                break;
+            }
+        }
+        while (j >= 0) {
+            if (t[j] === '#') {
+                tSkip++;
+                j--;
+            } else if (tSkip > 0) {
+                tSkip--;
+                j--;
+            } else {
+                break;
+            }
+        }
+        if (i >= 0 && j >= 0 && s[i] != t[j]) {
+            return false
+        }
+        if ((i >= 0) != (j >= 0)) {
+            return false
+        }
+        i--;
+        j--;
+    }
+    return true
 };
 /*
 Example 1:
